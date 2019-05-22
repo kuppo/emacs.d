abf876341ddd8b2a834e4402e39a38d14293caf5
Peng Lei <swk-pl@163.com>
2019-05-22 21:00:28 +0800

    add doom modeline, it is good looking for the mode line

---
+++ site-lisp/init-doommodeline.el
@@ -0,0 +1,34 @@
+(use-package doom-modeline
+  :hook (after-init . doom-modeline-mode)
+  :config
+  ;; Determines the style used by `doom-modeline-buffer-file-name'.
+  ;;
+  ;; Given ~/Projects/FOSS/emacs/lisp/comint.el
+  ;;   truncate-upto-project => ~/P/F/emacs/lisp/comint.el
+  ;;   truncate-from-project => ~/Projects/FOSS/emacs/l/comint.el
+  ;;   truncate-with-project => emacs/l/comint.el
+  ;;   truncate-except-project => ~/P/F/emacs/l/comint.el
+  ;;   truncate-upto-root => ~/P/F/e/lisp/comint.el
+  ;;   truncate-all => ~/P/F/e/l/comint.el
+  ;;   relative-from-project => emacs/lisp/comint.el
+  ;;   relative-to-project => lisp/comint.el
+  ;;   file-name => comint.el
+  ;;   buffer-name => comint.el<2> (uniquify buffer name)
+  ;;
+  ;; If you are expereicing the laggy issue, especially while editing remote files
+  ;; with tramp, please try `file-name' style.
+  ;; Please refer to https://github.com/bbatsov/projectile/issues/657.
+  (setq doom-modeline-buffer-file-name-style 'truncate-from-project)
+
+  ;; Whether display icons in mode-line or not.
+  (setq doom-modeline-icon t)
+
+  ;; Whether display minor modes in mode-line or not.
+  (setq doom-modeline-minor-modes t)
+
+  ;; Don’t compact font caches during GC.
+  (setq inhibit-compacting-font-caches t))
+
+
+
+(provide 'init-doommodeline)
