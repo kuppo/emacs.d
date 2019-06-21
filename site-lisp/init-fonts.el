9e42321b8cb01495eefa5b93c9ebfc15fbc004a5
Peng Lei <swk-pl@163.com>
2019-06-21 22:52:13 +0800

    change font scale for Chinese, make it looks better in Org mode

--- site-lisp/init-fonts.el
+++ site-lisp/init-fonts.el
@@ -1,11 +1,17 @@
 ;; Default font
-(set-default-font "Consolas 11")
+(set-default-font "Hack 10")
+;; (set-default-font "Consolas 11")
 
 ;; Chinese font
-(set-fontset-font t 'han (font-spec :family "Sarasa Gothic SC" :size 11))
+(set-fontset-font t 'han (font-spec :family "KaiTi" :size 10))
 
 ;; Chinese font needs to rescale
-(setq face-font-rescale-alist '(("Sarasa Gothic" . 1.5)))
+(setq face-font-rescale-alist '(("Sarasa Gothic" . 1.6)
+                                ("Microsoft Yahei" . 1.6)
+                                ("Microsoft JhengHei" . 1.6)
+                                ("STKaiti" . 1.6)
+                                ("KaiTi" . 1.6)
+                                ))
 
 
 (provide 'init-fonts)
