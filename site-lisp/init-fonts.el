da94435b8c0a79992abc6cd7abda131f97f48e8c
Ray <damntheft@gmail.com>
2019-11-22 17:19:43 +0800

    Update init-fonts.el

--- site-lisp/init-fonts.el
+++ site-lisp/init-fonts.el
@@ -1,54 +1,47 @@
+;;; package --- Summary
+
+;;; Commentary
+
+;;; Code:
 ;; Default coding system
 (set-default-coding-systems 'utf-8)
 
-;; A function could easily set font from the font list, both for English and Chinese.
-;; Thanks @强者 (see http://zhuoqiang.me/torture-emacs.html)
+;; Set font size
+;(let ((font-size 12)
+                                        ;      (cn-font-scale 1.2)))
+(setq font-size 12)
+(setq cn-font-scale 1.2)
 
-(defun c-font-existsp (font)            ;see if the font exists.
-  (if (null (x-list-fonts font))
-      nil
-    t))
+(when (display-graphic-p)
+  ;; Set default font
+  (catch 'loop
+    (dolist (font '("Consolas" "Hack" "Monaco" "DejaVu Sans Mono"
+                    "Monospace" "Courier New"))
+      (when (member font (font-family-list))
+        (set-face-attribute 'default nil :font font :height (* 10 font-size))
+        (throw 'loop t))))
 
-(require 'cl)
-(defun c-set-font (english-fonts        ;set the font function.
-                       english-font-size
-                       chinese-fonts
-                       &optional chinese-font-size)
-  (let ((en-font (format "%s %s"
-                         (find-if #'c-font-existsp english-fonts)
-                         english-font-size))
-        (zh-font (find-if #'c-font-existsp chinese-fonts)))
+  ;; Specify font for all unicode characters
+  (catch 'loop
+    (dolist (font '("Symbola" "Apple Symbols" "Symbol"))
+      (when (member font (font-family-list))
+        (set-fontset-font t 'unicode font nil 'prepend)
+        (throw 'loop t))))
 
-    ;; Set the default English font
-    ;;
-    ;; The following 2 method cannot make the font settig work in new frames.
-    ;; (set-default-font "Consolas:pixelsize=18")
-    ;; (add-to-list 'default-frame-alist '(font . "Consolas:pixelsize=18"))
-    ;; We have to use set-face-attribute
-     (set-face-attribute 'default nil :font en-font)
-
-    ;; Set Chinese font
-    ;; Do not use 'unicode charset, it will cause the English font setting invalid
-    (dolist (charset '(kana han symbol cjk-misc bopomofo))
-      (set-fontset-font (frame-parameter nil 'font)
-                        charset (font-spec :family zh-font)))))
-
-;; English font list
-(defvar c-en-list '("Consolas" "Hack" "Monaco" "DejaVu Sans Mono" "Monospace" "Courier New"))
-
-;; Chinese font list
-(defvar c-ch-list '("Microsoft Yahei" "Microsoft JhengHei" "文泉驿等宽微米黑" "黑体" "新宋体" "宋体"))
-
-;; Set proper font from above list
-(c-set-font c-en-list 12 c-ch-list)
+  ;; Specify font for Chinese characters
+  (catch 'loop
+    (dolist (font '("Microsoft Yahei" "Microsoft JhengHei"
+                    "文泉驿等宽微米黑" "黑体" "新宋体" "宋体"))
+      (when (member font (font-family-list))
+        (set-fontset-font t '(#x4e00 . #x9fff) font)
+        (throw 'loop t)))))
 
 ;; Chinese font needs to rescale
-(setq face-font-rescale-alist '(("Microsoft Yahei" . 1.2)
-                                ("Microsoft JhengHei" . 1.2)
-                                ("STKaiti" . 1.2)
-                                ("KaiTi" . 1.2)))
+(setq face-font-rescale-alist '(("Microsoft Yahei" . 1.8)
+                                ("Microsoft JhengHei" . 1.8)
+                                ("STKaiti" . 1.8)
+                                ("KaiTi" . 1.8)))
 
 
 (provide 'init-fonts)
 ;;; init-fonts.el ends here
-
